e6ccf4b885ff302c2256a637b5eb7988cbf01392
Michael Tan <mdtan@tudelft.net>
2020-05-27 15:32:15 +0200

    Added stream component functionality

--- aspenauto/main.py
+++ aspenauto/main.py
@@ -1,6 +1,10 @@
 import os
 import win32com.client as win32
 
+from .objectcollection import ObjectCollection
+from .blocks import Blocks
+from .streams import Material, Heat
+
 class Aspen(object):
 
 
@@ -8,8 +12,42 @@ class Aspen(object):
 
         self.aspen = win32.Dispatch('Apwn.Document')
         self.aspen.InitFromArchive2(os.path.abspath(Aspen_file))
+
+        self.blocks = ObjectCollection()
+        self.streams = ObjectCollection()
+        self.material_streams = ObjectCollection()
+        self.heat_streams = ObjectCollection()
         
+        blocks = self.aspen.Tree.FindNode("\Data\Blocks")
+        streams = self.aspen.Tree.FindNode("\Data\Streams")
+
+        for obj in blocks.Elements:
+            self.blocks[obj.Name] = obj
+
+        for obj in streams.Elements:
+            if hasattr(obj.Output, 'MASSFLMX'):
+                self.streams[obj.Name] = Material(obj)
+                self.material_streams[obj.Name] = Material(obj)
+
+            else:
+                self.streams[obj.Name] = Heat(obj)
+                self.heat_streams[obj.Name] = Heat(obj)
+
+
+
     def Run(self):
 
+        self.aspen.Engine.Run2()
+
+
+        blocks = self.aspen.Tree.FindNode("\Data\Blocks")
+        streams = self.aspen.Tree.FindNode("\Data\Streams")
+
+        for obj in blocks.Elements:
+            self.blocks[obj.Name] = obj
+
+
+        for obj in streams.Elements:
+            self.streams[obj.Name].Collect(obj)
 
         return
--- aspenauto/streams.py
+++ aspenauto/streams.py
@@ -1,8 +1,65 @@
-
+from .objectcollection import ObjectCollection
 
 class Streams(object):
 
     def __init__(self):
 
-
         return
+
+
+
+class Material(Streams):
+
+    def __init__(self, stream):
+        
+        self.pressure = []
+        self.temperature = []   
+        self.volflow = []
+
+        self.massfrac = ObjectCollection()
+        self.massflow = ObjectCollection()
+        self.moleflow = ObjectCollection()
+
+        if 'F-' in stream.Name:
+            self.type = 'Feed'
+        elif 'P-' in stream.Name:
+            self.type = 'Product'
+        elif 'W-' in stream.Name:
+            self.type = 'Waste'
+        else:
+            self.type = 'Standard'
+
+
+    def Collect(self, stream):
+        
+        self.temperature = stream.Output.TEMP_OUT.MIXED.Value
+        self.pressure = stream.Output.PRES_OUT.MIXED.Value
+        self.volflow = stream.Output.VOLFLMX.MIXED.Value
+
+        for obj in stream.Output.MASSFRAC.MIXED.Elements:
+            self.massfrac[obj.Name] = obj.Value
+
+        for obj in stream.Output.MASSFLOW.MIXED.Elements:
+            self.massflow[obj.Name] = obj.Value
+        self.massflow['total'] = stream.Output.MASSFLMX.MIXED.Value
+
+        for obj in stream.Output.MOLEFLOW.MIXED.Elements:
+            self.moleflow[obj.Name] = obj.Value
+        self.moleflow['total'] = stream.Output.MOLEFLMX.MIXED.Value
+
+
+
+class Heat(Streams):
+
+    def __init__(self, stream):
+
+    
+        self.Q = []
+        self.type = 'Thermal'
+
+    def Collect(self, stream):
+
+        self.Q = stream.Output.QCALC.Value
+
+    
+        
