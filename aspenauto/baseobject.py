b68cd6096c571348411f5203807d633ccfef4388
michaeltan91 <m.d.tan91@gmail.com>
2021-03-15 16:45:07 +0100

    Rename prop to key

--- aspenauto/baseobject.py
+++ aspenauto/baseobject.py
@@ -25,40 +25,40 @@ class BaseObject(object):
     def set_obj_value_frac(self, prop_loc, value):
         raise NotImplementedError
 
-    def __getattr__(self, prop):
-        if prop in self.properties.keys():
-            return self.get_property(prop)
-        elif prop in self.properties_frac.keys():
-            return self.get_property_frac(prop)
+    def __getattr__(self, key):
+        if key in self.properties.keys():
+            return self.get_property(key)
+        elif key in self.properties_frac.keys():
+            return self.get_property_frac(key)
         else:
-            raise AttributeError('Nonexistant Attribute', prop)
+            raise AttributeError('Nonexistant Attribute', key)
                 
-    def __setattr__(self, prop, value):
-        if prop in self.properties_in.keys():
-            self.set_property(prop, value)
-        elif prop in self.properties_frac_in.keys():
-            self.set_property_frac(prop, value)
+    def __setattr__(self, key, value):
+        if key in self.properties_in.keys():
+            self.set_property(key, value)
+        elif key in self.properties_frac_in.keys():
+            self.set_property_frac(key, value)
         else:
-            super(BaseObject, self).__setattr__(prop, value)
+            super(BaseObject, self).__setattr__(key, value)
 
-    def get_property(self, prop):
-        if prop not in self._values.keys():
-            prop_loc = self.properties[prop]
-            self._values[prop] = self.get_obj_value(prop_loc)
-        return self._values[prop]
+    def get_property(self, key):
+        if key not in self._values.keys():
+            prop_loc = self.properties[key]
+            self._values[key] = self.get_obj_value(prop_loc)
+        return self._values[key]
 
-    def get_property_frac(self, prop):
-        if prop not in self._values.keys():
-            prop_loc = self.properties_frac[prop]
-            self._values[prop] = self.get_obj_value_frac(prop_loc)
-        return self._values[prop]
+    def get_property_frac(self, key):
+        if key not in self._values.keys():
+            prop_loc = self.properties_frac[key]
+            self._values[key] = self.get_obj_value_frac(prop_loc)
+        return self._values[key]
 
-    def set_property(self, prop, value):
-        prop_loc = self.properties_in[prop]
+    def set_property(self, key, value):
+        prop_loc = self.properties_in[key]
         self.set_obj_value(prop_loc, value)
 
-    def set_property_frac(self, prop, value):
-        prop_loc = self.properties_frac_in[prop]
+    def set_property_frac(self, key, value):
+        prop_loc = self.properties_frac_in[key]
         self.set_obj_value_frac(prop_loc, value)
 
     
